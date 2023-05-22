2f389a909fb4181d2e2163545d493f3528aa6718
MohAitMesskine <aitmeskine.mohamed@gmail.com>
2023-05-22 17:06:05 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  
+
+  <p class="text-center">
+  <router-link   class="text-gray-300 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium" to="/"> Home</router-link>
+  <router-link   class="text-gray-300 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium" to="/about"> About</router-link>
+  </p>
+    <router-view></router-view>
+</template>
+
+<style scoped>
+
+</style>
